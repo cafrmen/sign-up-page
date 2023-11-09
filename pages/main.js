e799e586a05477ddad81f8209e38d631e108eeb5
cafrmen <cafrmen@gmail.com>
2023-11-09 15:38:52 -0600

    Finish assignment

--- pages/main.js
+++ pages/main.js
@@ -1,11 +1,13 @@
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm_password');
-const text = document.querySelector('.hidden');
+const text = document.querySelector('.show');
 const button = document.getElementById('button');
 
 button.addEventListener('click', (event) => {
     if (password.value !== confirmPassword.value) {
         event.preventDefault();
-        text.classList.add('show');
+        text.innerHTML = '* Passwords do not match';
+        password.value = '';
+        confirmPassword.value = '';
     }
 });
